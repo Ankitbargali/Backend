2bcf37e8652980d267b9ae48b4d3c432818a12aa
AnkitBargali <bargali.ankit@example.com>
2025-11-15 11:28:03 +0530

    backend initial commit

---
+++ Middelware/authMiddelware.js
@@ -0,0 +1,20 @@
+const jwt = require("jsonwebtoken");
+
+const authMiddelware = (req, res, next) => {
+  const token = req.cookies.token;
+
+  if (!req.cookies || !req.cookies.token) {
+    return res
+      .status(401)
+      .json({ message: "Access denied. No token provided." });
+  }
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded; // Attach decoded userId to request object
+    next();
+  } catch (error) {
+    res.status(403).json({ message: "Invalid or expired token" });
+  }
+};
+
+module.exports = authMiddelware;
